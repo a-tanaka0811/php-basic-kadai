51c3d010c5e32547514ea0d11e536faad56f4486
a-tanaka0811 <deshadesha0811@gmail.com>
2025-12-14 21:24:34 +0900

    kadai_007を追加

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <title>PHP基礎編</title>
+</head>
+
+<body>
+  <?php
+  // 連想配列
+  $vegetables = ['name' => 'onion', 'price' => 200, 'weight' => 160];
+
+  // 連想配列を出力
+  print_r($vegetables);
+  
+  ?>
+</body>
+</html>
