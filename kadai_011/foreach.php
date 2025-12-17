169cf00e1f7166acc6d4dc5b974f0c24fecf13ad
a-tanaka0811 <deshadesha0811@gmail.com>
2025-12-17 20:37:42 +0900

    kadai_011を追加

---
+++ kadai_011/foreach.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <title>PHP基礎編</title>
+</head>
+
+<body>
+  <?php
+  $product_data = ['名前' => '玉ねぎ', '値段' => 200, '産地' => '北海道'];
+
+  foreach ($product_data as $key => $value) {
+    // キーと値を出力
+    echo "{$key}：{$value}<br>";
+  }
+  ?>
+
+</body>
+</html>
