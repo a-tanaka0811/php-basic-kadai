4949582d1c396e8ca5130408d8ec5405358fc30b
a-tanaka0811 <deshadesha0811@gmail.com>
2025-12-19 16:21:34 +0900

    Add files via upload
    kadai_sortを追加

---
+++ kadai_sort/sort.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <title>PHP基礎編</title>
+</head>
+
+<body>
+    <p>
+        <?php
+        // ソートする配列を宣言
+        $nums = [15, 4, 18, 23, 10 ];
+
+        // 昇順ソート
+        sort_2way($nums, true);
+
+        // 降順ソート
+        sort_2way($nums, false);
+
+        // 関数定義
+        function sort_2way ($array, $order) {
+          if ($order === true) {
+            echo '昇順にソートします。<br>';
+            sort($array);
+            foreach ($array as $data) {
+              echo $data . '<br>';
+            }
+          }
+          else {
+            echo '降順にソートします。<br>';
+            rsort($array);
+            foreach ($array as $data) {
+              echo $data . '<br>';
+            }
+          }
+
+        }
+        ?>
+    </p>
+</body>
+
+</html>
